a5e64c9757d64212d8e4dfb7d8a8bbbf396d6e7e
Nikita Makarov <SurfStudio>
2016-09-10 15:46:07 +0400

    Portfolio section added. Translated into russian.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,29 @@
-$( document ).ready(function() {
-
-  //
-
+$(document).ready(function() {
+	smoothScroll(300);
+	portfolioSlide();
 });
+
+	function smoothScroll (duration) {
+		$('a[href^="#"]').on('click', function(event){
+			var target = $( $(this).attr('href') );
+
+			if( target.length ) {
+				event.preventDefault();
+				$('html, body').animate({
+					scrollTop: target.offset().top
+				}, duration);
+			}
+		});
+	}
+
+	function portfolioSlide(){
+		$('.thumb-unit').click(function(){
+			$('.portfolio-slider').css('left','-100%');
+			$('.work-container').show();
+		});
+		
+		$('.work-return').click(function(){
+			$('.portfolio-slider').css('left','0%');
+			$('.work-container').hide(800);
+		});
+	}
